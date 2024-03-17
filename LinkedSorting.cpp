8e71291ca8a335d33caa2570e8e6cbb49f25ced7
Edwin Shaji <edwinshajioffical@gmail.com>
2024-03-17 15:55:25 +0530

    Defined the Function for Creating the Linked List

--- LinkedSorting.cpp
+++ LinkedSorting.cpp
@@ -18,3 +18,32 @@ class Link
         void displayNode();
         void sortNode();
 };
+
+//function for creating linked list
+void Link :: createNode()
+{
+    NODE *prev,*q;
+    int i,n;
+
+    cout<<"\nEnter the of Node in a Linked List : ";
+    cin>>n;
+
+    p=new NODE;
+    cout<<"\nEnter the Data of Node no. 1 : ";
+    cin>>p->item;
+    p->next=NULL;
+
+    prev=p;
+
+    for(i=1;i<n;i++)
+    {
+        q=new NODE;
+
+        cout<<"\nEnter the Data of Node no. "<<i+1<<" : ";
+        cin>>q->item;
+        q->next=NULL;
+        prev->next=q;
+        prev=q;
+    }
+}
+
